9cef73bb9d675da2a6436225a985d8e3fc260c80
Adrian Czarnowski <adriannnczarnowski@gmail.com>
2024-04-16 12:46:06 +0200

    sql changes

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  user_id SERIgit AL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR (50) UNIQUE NOT NULL,
   password VARCHAR (50) NOT NULL,
   email VARCHAR (255) UNIQUE NOT NULL,
--- src/main/resources/db/migration/V3__newTables.sql
+++ src/main/resources/db/migration/V3__newTables.sql
@@ -18,12 +18,12 @@ CREATE TABLE budgets (
   user_id SERIAL,
   category_id SERIAL,
   amount FLOAT,
-  month MONTH NOT NULL,
-  year YEAR NOT NULL,
-  CONSTRAINT fk_user_id
+  month_dt DATE NOT NULL,
+  year_dt DATE NOT NULL,
+  CONSTRAINT f_user_id
     FOREIGN KEY(user_id)
         REFERENCES users(user_id),
-  CONSTRAINT fk_category_id
+  CONSTRAINT f_category_id
     FOREIGN KEY(category_id)
         REFERENCES categories(category_id)
 );
@@ -33,8 +33,8 @@ CREATE TABLE reports (
   report_type VARCHAR(50) NOT NULL,
   start_time DATE NOT NULL,
   end_time DATE NOT NULL,
-  generated_date TIMESTAMP NOT NULL
-  CONSTRAINT fk_user_id
+  generated_date TIMESTAMP NOT NULL,
+  CONSTRAINT kf_user_id
     FOREIGN KEY(user_id)
         REFERENCES users(user_id)
 );
