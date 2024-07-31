4941120dfe7d39a4e55cbc737beabb1a59b2baa4
Adrian Czarnowski <adriannnczarnowski@gmail.com>
2024-07-31 08:06:42 +0200

    adding Transactional Event listener

--- src/main/java/projects/adrian/homebudget/event/MyEventListener.java
+++ src/main/java/projects/adrian/homebudget/event/MyEventListener.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -18,4 +19,10 @@ public class MyEventListener {
     public void onBudgetChangeEvent(BudgetChangeEvent budgetChangeEvent){
         log.info("Budget change event, message from listener " + budgetChangeEvent.getBudgetDto().budgetId());
     }
+
+    @TransactionalEventListener(BudgetChangeEvent.class)
+    public void onBudgetChangeBasedOnTransaction(BudgetChangeEvent budgetChangeEvent){
+        log.info("Budget change event, message from listener " + budgetChangeEvent.getBudgetDto().budgetId());
+    }
 }
+
